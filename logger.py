81877d1912b74fc2e521d2ff76408ef967adc64f
Joel Truher <truher@google.com>
2020-04-11 20:46:21 -0700

    logger now hot pluggable

--- logger.py
+++ logger.py
@@ -1,19 +1,43 @@
-import datetime, serial, string
+#!/usr/bin/python3
+import datetime, glob, serial, sys
 
-def readit(idx, ser):
-    r = ser.readline()
-    if r != "":
-        f = r.split()
-        id = f[0]
-        print(datetime.datetime.now(),id," ".join(f))
-    
-def main():
-    ser0 = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=20)
-    ser1 = serial.Serial('/dev/ttyACM1', 9600, 8, 'N', 1, timeout=20)
+def readit(ser, f):
+    try:
+        r = ser.readline().rstrip().decode('ascii')
+        if len(r) > 0:
+            print(f'"{datetime.datetime.now()}" {r}', file=f)
+            f.flush()
+    except serial.serialutil.SerialException:
+        print("fail", ser.port, file=sys.stderr)
+        ser.close()
+        # mark it dead
+        ser.port = ""
 
+serials = []
+
+with open('data.csv', 'a') as f:
     while True:
-        readit(0, ser0)
-        readit(1, ser1)
-
-if __name__ == "__main__":
-    main()
+        try:
+            # remove the dead serials (marked with empty ports)
+            serials = [*filter(lambda x: len(x.port)>0, serials)]
+    
+            # remove serials without matching ttys
+            current_ttys = glob.glob("/dev/ttyACM*")
+            serials = [*filter(lambda x: x.port in current_ttys, serials)]
+    
+            # add new serials
+            current_ports = [*map(lambda x: x.port, serials)]
+            ttys_without_ports = [*set(current_ttys).difference(current_ports)]
+            if len(ttys_without_ports) > 0:
+                print("connecting", ttys_without_ports, file=sys.stderr)
+            serials.extend(
+                [*map(lambda f: serial.Serial(f, 9600, 8, 'N', 1, timeout=1),
+                      ttys_without_ports)])
+    
+            # read from all the serials
+            [*map(lambda x: readit(x,f), serials)]
+    
+        except KeyboardInterrupt:
+            raise
+        except:
+            print("top level exception",sys.exc_info()[0], file=sys.stderr)
