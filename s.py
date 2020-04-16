77694c130f52a9dd084b1308723e9d587d821fed
Joel Truher <truher@google.com>
2020-04-15 21:58:29 -0700

    minor fixes

--- s.py
+++ s.py
@@ -17,7 +17,7 @@ def random_data():
     time_deltas  = pd.to_timedelta(np.random.uniform(-1, 1, num_rows),unit='S')
     time_actual = time_ideal + time_deltas
 
-    #data  = np.random.uniform(0, 1, num_rows)
+    #data  = np.random.uniform(0.99, 1.01, num_rows)
     # lognormal(0,1) has mean exp(0.5) or about 1.65
     data  = np.random.lognormal(0, 1, num_rows)
 
@@ -26,39 +26,32 @@ def random_data():
     return df
 
 def make_hourly(raw_data):
-    # Bucket boundaries we want, with some left padding
-    buckets = pd.DataFrame(pd.date_range(start=raw_data.index.min().floor('10T') - 2 * pd.offsets.Minute(10),
-                  end=raw_data.index.max().ceil('10T'), freq='10T')).set_index(0)
-    #print("got buckets")
-    #print(buckets.tail())
+    # provide a zero just before the first point, so integration sees the first
+    # point but nothing before it
+    raw_data = pd.concat(
+        [pd.DataFrame(index=[raw_data.index.min() - pd.DateOffset(seconds=1)],
+                      data=[0], columns=['measure']), raw_data])
+
+    # Bucket boundaries we want, with some left padding to be sure we can set the first to zero
+    buckets = pd.DataFrame(pd.date_range(start=raw_data.index.min().floor('H') - pd.DateOffset(hours=1),
+                  end=raw_data.index.max().ceil('H'), freq='H')).set_index(0)
 
     # Insert bucket boundaries into the raw dataset (they'll have NaN measures)
     raw_data_with_buckets = raw_data.append(buckets).sort_index()
 
     # Set the left edge to zero
     raw_data_with_buckets.at[raw_data_with_buckets.index.min()]=0
-    #print("got raw with buckets")
-
 
     # Fill interpolated values at the bucket boundaries
-    interpolated = raw_data_with_buckets.interpolate(method='time').dropna()
-    #print("got interpolated")
-    #print(interpolated)
+    interpolated = raw_data_with_buckets.interpolate(method='time', limit_area='inside').dropna()
 
     # Integrate the data series to get cumulative energy (kWh)
     cum_kwh = pd.DataFrame(index=interpolated.index, columns=['measure'],
                   data=it.cumtrapz(interpolated['measure'],
                                    x=interpolated.index, initial=0) / (1000 * np.timedelta64(1, 'h')))
-    #print("got cumtrapz")
-    #print(cum_kwh)
 
     # Downsample to the buckets
-    #downsampled = interpolated.resample(rule='10T',closed='right',label='right',loffset='-10T').max()
-    cum_kwh_10T = cum_kwh.resample(rule='10T',closed='right',label='right',loffset='-10T').max()
-    #print("got cum_kwh_10T")
-    #print(cum_kwh_10T)
-
-    hourly = cum_kwh_10T.resample(rule='H').max().diff()
+    hourly = cum_kwh.resample(rule='H',closed='right',label='right',loffset='-1H').max().diff().dropna()
     return hourly
 
 def plot_raw_data(raw_data, fig):
@@ -75,7 +68,7 @@ def plot_rollups(hourly, fig):
     ax.set_title('kWh by hour (168 hours)')
     ax.set_ylabel('kilowatt-hours')
     (hourly
-        .loc[hourly.index > pd.Timestamp.now() - pd.DateOffset(hours=168)]
+        .loc[hourly.index > (pd.Timestamp.now() - pd.DateOffset(hours=168)).ceil('H')]
         .resample(rule='H').sum()
         .plot(ax=ax, style='o', legend=False)
     )
@@ -84,7 +77,7 @@ def plot_rollups(hourly, fig):
     ax.set_title('kWh by day (31 days)')
     ax.set_ylabel('kilowatt-hours')
     (hourly
-         .loc[hourly.index > pd.Timestamp.now() - pd.DateOffset(days=31)]
+         .loc[hourly.index > (pd.Timestamp.now() - pd.DateOffset(days=31)).ceil('D')]
          .resample(rule='D').sum()
          .plot(ax=ax, style='o', legend=False)
     )
@@ -92,8 +85,9 @@ def plot_rollups(hourly, fig):
     ax = fig.add_subplot(4,1,4)
     ax.set_title('kWh by month (12 months)')
     ax.set_ylabel('kilowatt-hours')
+    # month needs special treatment since it's variable freq
     (hourly
-        .loc[hourly.index > pd.Timestamp.now() - pd.DateOffset(months=12)]
+        .loc[hourly.index > (pd.Timestamp.now() - pd.DateOffset(months=12)).to_datetime64().astype('<M8[M]')]
         .resample(rule='MS').sum()
         .plot(ax=ax, style='o', legend=False)
     )
