98a68e34fbf8bf3a339eb262b1fcd43542f4a654
Joel Truher <truher@google.com>
2020-04-15 18:09:35 -0700

    fiddling with plotting

--- t.py
+++ t.py
@@ -1,7 +1,7 @@
 import io
 import numpy as np
 import pandas as pd
-import random
+#import random
 import scipy.integrate as it
 import webbrowser
 
@@ -11,18 +11,44 @@ from matplotlib.figure import Figure
 from waitress import serve
 
 app = Flask(__name__)
+ids = [ "5737333034370D0E14", "5737333034370A220D" ]
 
-@app.route("/")
-def index():
+def read_file():
     # Read the raw file (watts)
     raw_data = pd.read_csv('dt.0', delim_whitespace=True, comment='#')
     raw_data['time'] = pd.to_datetime(raw_data['time'])
     raw_data = raw_data.set_index(['time'])
+    return raw_data
+
+def random_data():
+    num_rows = 1000000
+
+    time_ideal = pd.date_range(end=pd.Timestamp.now(), periods=num_rows, freq='10S')
+    time_deltas  = pd.to_timedelta(np.random.uniform(-1, 1, num_rows),unit='S')
+    time_actual = time_ideal + time_deltas
+
+    #data  = np.random.uniform(0, 1, num_rows)
+    # lognormal(0,1) has mean exp(0.5) or about 1.65
+    data  = np.random.lognormal(0, 1, num_rows)
+
+    df = pd.DataFrame(data={'time': time_actual, 'measure': data})
+    df = df.set_index(['time'])
+    return df
+
+@app.route("/")
+def index():
+
+    #raw_data = read_file()
+    raw_data = random_data()
+    print("got raw_data")
+    print(raw_data.tail())
 
     # Bucket boundaries we want, with some left padding
     buckets = pd.DataFrame(pd.date_range(start=raw_data.index.min().floor('10T') - 2 * pd.offsets.Minute(10),
                        end=raw_data.index.max().ceil('10T'), freq='10T'))
     buckets = buckets.set_index(0)
+    print("got buckets")
+    print(buckets.tail())
 
     # Insert bucket boundaries into the raw dataset (they'll have NaN measures)
     raw_data_with_buckets = raw_data.append(buckets)
@@ -30,48 +56,124 @@ def index():
 
     # Set the left edge to zero
     raw_data_with_buckets.at[raw_data_with_buckets.index.min()]=0
+    print("got raw with buckets")
 
     # Fill interpolated values at the bucket boundaries
     interpolated = raw_data_with_buckets.interpolate(method='time')
     interpolated = interpolated.dropna()
+    print("got interpolated")
 
     # Integrate the data series to get cumulative energy (kWh)
     interpolated['cum'] = it.cumtrapz(interpolated['measure'],
                                       x=interpolated.index, initial=0) / (1000 * np.timedelta64(1, 'h'))
+    print("got cumtrapz")
 
     # Downsample to the buckets
     downsampled = interpolated.resample(rule='10T',closed='right',label='right',loffset='-10T').max()
+    print("got downsampled")
+    # roll up the 10 min samples to hours
+    hourly = downsampled.resample(rule='H').max()
+    print("got hourly")
+    print(hourly.tail())
+    # roll up the hours to days
+    daily = hourly.resample(rule='D').max()
+    print("got daily")
+    print(daily.tail())
+    # roll up the days to months
+    monthly = daily.resample(rule='MS').max()
+    print("got monthly")
+    print(monthly)
 
     # Differentiate to get mean power by bucket (watts)
-    downsampled['cumdiff'] = downsampled['cum'].diff()*60000/10
+    # * 1000 (kw -> w)
+    # * 60   (h -> min)
+    # / 10   (min -> 10min)
+    # => * 6000
+    # todo: this does the current period wrong
+    downsampled['cumdiff'] = downsampled['cum'].diff()*6000
     downsampled = downsampled.dropna()
+    print("got diff")
+    print(downsampled.tail())
+
+    # * 1000 (kw -> w)
+    # todo: this does the current hour wrong
+    hourly['cumdiff'] = hourly['cum'].diff()*1000
+    hourly = hourly.dropna()
+    print("got hourly")
+    print(hourly.tail())
+
+    # * 1000 (kw -> w)
+    # / 24   (h -> d)
+    # todo: this does the current day wrong
+    daily['cumdiff'] = daily['cum'].diff()*1000/24
+    daily = daily.dropna()
+    print("got daily")
+    print(daily.tail())
+
+    # * 1000 (kw -> w)
+    # / 24   (h -> d)
+    # / days-in-month (d -> m)
+    # todo: this does the current month wrong
+    monthly['cumdiff'] = monthly['cum'].diff()*1000/(24 * monthly.index.days_in_month)
+    monthly = monthly.dropna()
+    print("got monthly days")
+    print(monthly.index.days_in_month)
+    print("got monthly")
+    print(monthly.tail())
 
     # Plot the results
-    fig = Figure(figsize=(10,10))
+    fig = Figure(figsize=(10,20))
 
-    ax = fig.add_subplot(4,1,1)
-    ax.set_title('observed power (raw)')
+    ax = fig.add_subplot(7,1,1)
+    ax.set_title('observed power (raw) should cover 3h')
     ax.set_xlabel('time')
     ax.set_ylabel('watts')
-    raw_data.plot(ax=ax, y='measure', style='o', legend=False)
+    raw_data.tail(1000).plot(ax=ax, y='measure', style='o', legend=False)
+    print("got plot1")
 
-    ax = fig.add_subplot(4,1,2)
-    ax.set_title('interpolated power (10 min periods)')
+    # todo: make fine-grained view shorter
+    ax = fig.add_subplot(7,1,2)
+    ax.set_title('observed power with interpolated bucket boundaries (10 min periods) should cover 7d')
     ax.set_xlabel('time')
     ax.set_ylabel('watts')
-    interpolated.plot(ax=ax, y='measure', style='o', legend=False)
+    interpolated.tail(1000).plot(ax=ax, y='measure', style='o', legend=False)
+    print("got plot2")
 
-    ax = fig.add_subplot(4,1,3)
-    ax.set_title('cumulative energy (10 min periods)')
+    ax = fig.add_subplot(7,1,3)
+    ax.set_title('cumulative energy (10 min periods) should cover 7d')
     ax.set_xlabel('time')
     ax.set_ylabel('kilowatt-hours')
-    downsampled.plot(ax=ax, y='cum', style='o', legend=False)
+    downsampled.tail(1000).plot(ax=ax, y='cum', style='o', legend=False)
+    print("got plot3")
 
-    ax = fig.add_subplot(4,1,4)
-    ax.set_title('mean power (10 min periods)')
+    ax = fig.add_subplot(7,1,4)
+    ax.set_title('mean power (10 min periods) should cover 7d')
     ax.set_xlabel('time')
     ax.set_ylabel('watts')
-    downsampled.plot(ax=ax, y='cumdiff', style='o', legend=False)
+    downsampled.tail(1000).plot(ax=ax, y='cumdiff', style='o', legend=False)
+    print("got plot4")
+
+    ax = fig.add_subplot(7,1,5)
+    ax.set_title('mean power (1 h periods) should cover 1m')
+    ax.set_xlabel('time')
+    ax.set_ylabel('watts')
+    hourly.tail(1000).plot(ax=ax, y='cumdiff', style='o', legend=False)
+    print("got plot5")
+
+    ax = fig.add_subplot(7,1,6)
+    ax.set_title('mean power (1 d periods) should cover 3y')
+    ax.set_xlabel('time')
+    ax.set_ylabel('watts')
+    daily.tail(1000).plot(ax=ax, y='cumdiff', style='o', legend=False)
+    print("got plot6")
+
+    ax = fig.add_subplot(7,1,7)
+    ax.set_title('mean power (1 m periods) should cover 100y')
+    ax.set_xlabel('time')
+    ax.set_ylabel('watts')
+    monthly.tail(1000).plot(ax=ax, y='cumdiff', style='o', legend=False)
+    print("got plot7")
+
 
     # Make sure the titles don't overlap
     fig.set_tight_layout(True)
