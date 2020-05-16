d0893e2d830af746dff4894580477de67f57e43a
Joel Truher <truher@google.com>
2020-05-16 12:22:12 -0700

    linted arduino, found i was overrunning sprintf

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,3 +1,4 @@
+// Copyright 2020 Truher
 #include <avr/boot.h>
 
 enum Column {
@@ -5,7 +6,7 @@ enum Column {
 };
 Column column = VOLTS;
 
-char uidStr[20] = {0};
+char uidStr[19] = {0};
 const unsigned int ROWS = 1000;
 
 // observations from analogRead()
@@ -18,7 +19,7 @@ int row = 0;
 // ct sensor in [1,2,3,4]
 int ct = 1;
 
-char buf[2]; // for each observation byte
+char buf[3];  // for each observation byte
 
 // for delta encoding
 int v_first = 0;
@@ -30,19 +31,23 @@ int a_prev = 0;
 bool err = false;
 
 void setup() {
-  sprintf(uidStr,"%02X%02X%02X%02X%02X%02X%02X%02X%02X",
-    boot_signature_byte_get(14), boot_signature_byte_get(15), boot_signature_byte_get(16),
-    boot_signature_byte_get(17), boot_signature_byte_get(18), boot_signature_byte_get(19),
-    boot_signature_byte_get(21), boot_signature_byte_get(22), boot_signature_byte_get(23));
+  snprintf(uidStr, sizeof(uidStr), "%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+    boot_signature_byte_get(14), boot_signature_byte_get(15),
+    boot_signature_byte_get(16), boot_signature_byte_get(17),
+    boot_signature_byte_get(18), boot_signature_byte_get(19),
+    boot_signature_byte_get(21), boot_signature_byte_get(22),
+    boot_signature_byte_get(23));
   pinMode(LED_BUILTIN, OUTPUT);
 
   // set up Serial
   Serial.begin(115200);
   while (!Serial) {}
 
-  // set up ADC
-  ADCSRA &= ~(bit(ADPS0) | bit(ADPS1) | bit(ADPS2));  // clear prescale
-  ADCSRA |= bit(ADPS0) | bit(ADPS2);  // ADC prescale = 32, tradeoff between duration and accuracy
+  // Set up ADC
+  // Clear prescale
+  ADCSRA &= ~(bit(ADPS0) | bit(ADPS1) | bit(ADPS2));
+  // ADC prescale = 32, tradeoff between duration and accuracy
+  ADCSRA |= bit(ADPS0) | bit(ADPS2);
   analogRead(0);
 
   // initialize timer1
@@ -50,8 +55,9 @@ void setup() {
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1  = 0;
-  //OCR1A = 800;  // compare match register 16MHz/1/10khz/2 samples (50us) = 6 cycles per measurement
-  OCR1A = 1600;  // compare match register 16MHz/1/5khz/2 samples (100us) = 12 cycles per measurement
+  // Set compare match register
+  // OCR1A = 800;  // 16MHz/1/10khz/2 samples (50us) = 6 cycles per measurement
+  OCR1A = 1600;  // 16MHz/1/5khz/2 samples (100us) = 12 cycles per measurement
   TCCR1B |= (1 << WGM12);  // CTC mode
   TCCR1B |= (1 << CS10);  // prescale = 1
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
@@ -65,6 +71,7 @@ int aa = 0;
 ISR(TIMER1_COMPA_vect) {
   if (row >= ROWS) return;  // wait for printing to be done
 
+  // TODO(truher): remove the duplication here
   switch (column) {
     case VOLTS:
       v = analogRead(0);
@@ -103,15 +110,16 @@ ISR(TIMER1_COMPA_vect) {
       break;
   }
 
-  if(row >= ROWS)
+  if (row >= ROWS)
     TCCR1B = 0;  // stop timer
 }
 
 void loop() {
-  if (row < ROWS) return;  //wait for measurement to be done
+  if (row < ROWS) return;  // wait for measurement to be done
 
   digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ 1);
 
+  // TODO(truher): extract a serialize function, test it.
   Serial.print(err);
   Serial.print("\t");
   Serial.print(uidStr);
@@ -120,16 +128,16 @@ void loop() {
   Serial.print("\t");
   Serial.print(v_first);
   Serial.print("\t");
-  for(int r = 0; r < ROWS; ++r) {
-    sprintf(buf, "%02X", volts[r]);
+  for (int r = 0; r < ROWS; ++r) {
+    snprintf(buf, sizeof(buf), "%02X", volts[r]);
     volts[r] = 0;
     Serial.write(buf, 2);
   }
   Serial.print("\t");
   Serial.print(a_first);
   Serial.print("\t");
-  for(int r = 0; r < ROWS; ++r) {
-    sprintf(buf, "%02X", amps[r]);
+  for (int r = 0; r < ROWS; ++r) {
+    snprintf(buf, sizeof(buf), "%02X", amps[r]);
     amps[r] = 0;
     Serial.write(buf, 2);
   }
